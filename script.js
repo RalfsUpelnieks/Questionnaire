feb80912031ab8f6d8d1830c2e4085f0f1a31010
Ralfs Upelnieks <ralfs.upelnieks@gmail.com>
2023-07-31 15:40:26 +0300

    Added task

---
+++ script.js
@@ -0,0 +1,105 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('answer'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const endScreen = document.getElementById('endScreen');
+const game = document.getElementById('game');
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+let questions = [];
+
+//CONSTANTS
+const MAX_QUESTIONS = 10;
+
+fetch(
+    'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
+)
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuesions = [...questions];
+    getNewQuestion();
+    game.classList.remove('hidden');
+    endScreen.classList.add('hidden');
+};
+
+getNewQuestion = () => {
+    if (availableQuesions.length === 0) {
+        localStorage.setItem('mostRecentScore', score);
+        scoreText.innerHTML = `Congradulations! You answered ${score}/10<br/>questions correctly.`;
+        game.classList.add('hidden');
+        endScreen.classList.remove('hidden');
+        return;
+    }
+    questionCounter++;
+    progressText.innerText = `${questionCounter} / ${MAX_QUESTIONS}`;
+
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerHTML = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerHTML = currentQuestion['choice' + number];
+    });
+
+    availableQuesions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if (classToApply === 'correct') {
+            score += 1;
+        }
+
+        selectedChoice.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    });
+});
